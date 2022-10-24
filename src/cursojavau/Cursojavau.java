b456e0a42d7a5640dc0525cafb2c0aba929a4fb0
Mauricio Uñate Candela <67971172+FreewayVlogs@users.noreply.github.com>
2022-10-24 11:59:19 -0500

    ventanita prueba

--- src/cursojavau/Cursojavau.java
+++ src/cursojavau/Cursojavau.java
@@ -4,6 +4,8 @@
  */
 package cursojavau;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author mauriciounca
@@ -15,6 +17,8 @@ public class Cursojavau {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null, "login ok");
     }
     
 }
